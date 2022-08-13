5c0128abfd9c0f99f735e58431b9676ae5035bfa
Murilo Morandi Alexandre <murilomorandialexandre2011@gmail.com>
2022-08-13 02:02:35 -0300

    Entrega do desafio

--- src/components/Card/CardAnimation/index.tsx
+++ src/components/Card/CardAnimation/index.tsx
@@ -18,16 +18,15 @@ export function CardAnimation({ children, ...rest }: CardAnimationProps) {
   const cardOffset = useSharedValue(0.25 * displayWidth);
 
   const animatedStyle = useAnimatedStyle(() => {
-    return {
-      // TODO - setup animated style
+    return {      
+      opacity: cardOpacity.value,
+      transform: [{translateX: cardOffset.value}]
     }
   })
 
   useEffect(() => {
-    /**
-     * TODO - setup cardOpacity.value and cardOffset.value with
-     * withTiming()
-     */
+    cardOpacity.value = withTiming(1, { duration: 1000 })
+    cardOffset.value = withTiming(0, { duration: 1000 })
   }, []);
 
   return (
